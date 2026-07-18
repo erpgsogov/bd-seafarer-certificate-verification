5ee1e2aaf34c5600ade45cd843f96419ad258dcb
erpgsogov <marinertito1362@gmail.com>
2026-07-19 04:45:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+.result-box {
+    margin-top: 20px;
+    padding: 15px;
+    border-radius: 5px;
+    display: none;
+}
+.result-success {
+    background-color: #d4edda;
+    color: #155724;
+    border: 1px solid #c3e6cb;
+}
+.result-error {
+    background-color: #f8d7da;
+    color: #721c24;
+    border: 1px solid #f5c6cb;
+}
